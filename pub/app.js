cee3cc9975b6281ef58dcee38838c5b6c4810183
dannonb <dannonbigham@gmail.com>
2020-03-18 16:19:36 -0700

    new 2020 portfolio

---
+++ pub/app.js
@@ -0,0 +1,17 @@
+const game = {}
+
+const web = {}
+
+document.querySelector('#gamebtn').addEventListener('click', () => {
+
+})
+
+document.querySelector('#webbtn').addEventListener('click', () => {
+
+})
+
+document.querySelector('#contact').addEventListener('click', () => {
+    const bizCard = document.createElement('div')
+    
+    window.alert()
+})
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
+
+const pubDir = path.join(__dirname, '../pub')
+app.use(express.static(pubDir))
+
+app.listen(port)
