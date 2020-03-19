29275bf7131d4fbf5b418826c0a774f237da1ba2
dannonb <dannonbigham@gmail.com>
2020-03-19 10:46:29 -0700

    adding business card

--- pub/app.js
+++ pub/app.js
@@ -11,7 +11,13 @@ document.querySelector('#webbtn').addEventListener('click', () => {
 })
 
 document.querySelector('#contact').addEventListener('click', () => {
-    const bizCard = document.createElement('div')
+    const bizCard = document.querySelector('#business-card')
+    bizCard.style.display = "block"
     
-    window.alert()
+    
+})
+
+document.querySelector('.close').addEventListener('click', () => {
+    const bizCard = document.querySelector('#business-card')
+    bizCard.style.display = "none"
 })
