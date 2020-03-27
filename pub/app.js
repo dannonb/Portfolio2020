86d7f48f80d4a43a9dfdd7c5cfe0f53884b2f369
dannonb <dannonbigham@gmail.com>
2020-03-27 00:16:22 -0700

    making back buttons work

--- pub/app.js
+++ pub/app.js
@@ -6,12 +6,14 @@ document.querySelector('#gamebtn').addEventListener('click', () => {
     document.getElementById('start').remove()
     document.getElementById('gameArea').style.display = 'flex'
     document.querySelector('header').style.backgroundColor = 'paleturquoise'
+    document.querySelector('footer').style.backgroundColor = 'paleturquoise'
 })
 
 document.querySelector('#webbtn').addEventListener('click', () => {
     document.getElementById('start').remove()
     document.getElementById('webArea').style.display = 'flex'
     document.querySelector('header').style.backgroundColor = 'palevioletred'
+    document.querySelector('footer').style.backgroundColor = 'palevioletred'
 })
 
 document.querySelector('#contact').addEventListener('click', () => {
@@ -24,4 +26,14 @@ document.querySelector('#contact').addEventListener('click', () => {
 document.querySelector('.close').addEventListener('click', () => {
     const bizCard = document.querySelector('#business-card')
     bizCard.style.display = "none"
+})
+
+document.querySelector('#web-home').addEventListener('click', () => {
+    location.reload()
+    
+})
+
+document.querySelector('#game-home').addEventListener('click', () => {
+    location.reload()
+    
 })
