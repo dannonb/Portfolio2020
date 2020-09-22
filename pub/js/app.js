b0457cd0ed7ab8153250b14f2e8fb4522de361ae
dannonb <dannonbigham@gmail.com>
2020-09-22 08:14:49 -0700

    ignore node_modules

--- pub/js/app.js
+++ pub/js/app.js
@@ -1,39 +1,86 @@
-const game = {}
 
-const web = {}
+const about = document.querySelector('#about-me')
+const projects = document.querySelector('#projects')
+const certs = document.querySelector('#certs')
+const main = document.querySelector('#main')
 
-document.querySelector('#gamebtn').addEventListener('click', () => {
-    document.getElementById('start').remove()
-    document.getElementById('gameArea').style.display = 'flex'
-    document.querySelector('header').style.backgroundColor = 'paleturquoise'
-    document.querySelector('footer').style.backgroundColor = 'paleturquoise'
+const start = document.querySelector('#start')
+const starttext = document.querySelector('#msg')
+const msg = "Hello, welcome to my portfolio. Please click on a link or a button below to find out more information. You can also visit me on social media. Links to my socials can be found in the footer. Thanks!"
+
+
+const aboutbtn = document.querySelector('#aboutbtn')
+const projectbtn = document.querySelector('#projectbtn')
+const certsbtn = document.querySelector('#certsbtn')
+
+
+
+
+
+const startmsg = () => {
+    let msg1 = msg.split('')
+    let msg2 = ''
+
+    msg1.forEach((letter, i)=> {
+        setTimeout(() => {
+            msg2 += letter
+            starttext.textContent = msg2
+        }, i * 50)
+    })
+    setTimeout(() => {
+        aboutbtn.style.transition = 'all 2s ease'
+        aboutbtn.style.opacity = '1'
+        
+    }, 9000)
+    setTimeout(() => {
+        projectbtn.style.transition = 'all 2s ease'
+        projectbtn.style.opacity = '1'
+        
+    }, 9500)
+    setTimeout(() => {
+        certsbtn.style.transition = 'all 2s ease'
+        certsbtn.style.opacity = '1'
+        
+    }, 10000)
+
+
+}
+
+
+window.addEventListener('load', () => {
+    startmsg()
+
 })
 
-document.querySelector('#webbtn').addEventListener('click', () => {
-    document.getElementById('start').remove()
-    document.getElementById('webArea').style.display = 'flex'
-    document.querySelector('header').style.backgroundColor = 'palevioletred'
-    document.querySelector('footer').style.backgroundColor = 'palevioletred'
-})
 
-document.querySelector('#contact').addEventListener('click', () => {
-    const bizCard = document.querySelector('#business-card')
-    bizCard.style.display = "block"
-    
-    
-})
 
-document.querySelector('.close').addEventListener('click', () => {
-    const bizCard = document.querySelector('#business-card')
-    bizCard.style.display = "none"
-})
 
-document.querySelector('#web-home-btn').addEventListener('click', () => {
-    location.reload()
-    
-})
 
-document.querySelector('#game-home-btn').addEventListener('click', () => {
-    location.reload()
-    
-})
+
+const displayAbout = () => {
+    window.event.preventDefault()
+    start.remove()
+    main.querySelectorAll('section').forEach((element) => {
+        element.style.display = 'none'
+    })
+    about.style.display = 'block'
+}
+
+const displayProjects = () => {
+    window.event.preventDefault()
+    start.remove()
+    main.querySelectorAll('section').forEach((element) => {
+        element.style.display = 'none'
+    })
+    projects.style.display = 'block'
+}
+
+const displayCerts = () => {
+    window.event.preventDefault()
+    start.remove()
+    main.querySelectorAll('section').forEach((element) => {
+        element.style.display = 'none'
+    })
+    certs.style.display = 'block'
+}
+
